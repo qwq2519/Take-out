61df8cd0b21d6a67c76757b27bc4473087f307b9
2519 <2519167027@qq.com>
2025-03-18 18:31:49 +0800

    add:工作台模块业务

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -10,6 +10,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -46,4 +47,10 @@ public interface DishMapper {
     List<Dish> listBySetmealId(Long id);
 
     List<Dish> list(Dish dish);
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
